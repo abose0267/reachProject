b2834178913b4dd053681335525fe12467d0f20f
arvind-balaji <arvindb02@gmail.com>
2022-05-27 02:38:50 -0400

    Enable aliased imports
    ex: import "@app/components/Button" as opposed to "../components/Button"

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,30 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    env: {
+      production: {
+        plugins: ['react-native-paper/babel'],
+      },
+    },
+    plugins: [
+      [
+        'module-resolver',
+        {
+          extensions: [
+            '.js',
+            '.jsx',
+            '.ts',
+            '.tsx',
+            '.android.js',
+            '.android.tsx',
+            '.ios.js',
+            '.ios.tsx',
+          ],
+          alias: {'@app': './'},
+          // root: ['./src'],
+        },
+      ],
+    ],
   };
 };
