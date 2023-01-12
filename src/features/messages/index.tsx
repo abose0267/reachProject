a77cf415dd1963634fb905bf0fb4e7773bca3e2f
arvind-balaji <arvindb02@gmail.com>
2023-01-12 13:08:44 -0600

    Fix coflicts

--- src/features/messages/index.tsx
+++ src/features/messages/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Messages from './screens/Messages';
 import MessageList from './screens/MessageList';
 import Settings from '../settings/settings';
@@ -9,6 +9,76 @@ import GroupInfo from './screens/GroupInfo';
 import CreateBlast from '../blast/CreateBlast';
 import DraftBlast from '../blast/DraftBlast';
 import Pinned from './screens/Pinned';
+// import { Settings } from 'react-native';
+
+
+const Stack = createNativeStackNavigator();
+
+const MessagesStack = () => {
+  return (
+    <>
+      {/*// @ts-ignore */}
+      <Stack.Navigator initialRouteName='landing'>
+        <Stack.Screen
+          name="Messages"
+          component={MessageList}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="CreateGroup"
+          component={CreateGroup}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="CreateBlast"
+          component={CreateBlast}
+          options={{ headerShown: false }}
+        />
+         <Stack.Screen
+          name="DraftBlast"
+          component={DraftBlast}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="GroupInfo"
+          component={GroupInfo}
+          options={{ headerShown: false, presentation: 'modal' }}
+        />
+        <Stack.Screen
+          name="messages"
+          component={Messages}
+          options={{ headerShown: false }} 
+        />
+        <Stack.Screen 
+          name="Pinned"
+          component={Pinned}
+          options={{
+            headerShown: false,
+            presentation: 'modal'
+          }}
+        />
+        {/* <Stack.Screen 
+          name="readannouncements"
+          component={ReadAnnouncements}
+          options={{
+            headerShown: false, 
+            presentation: 'modal'
+          }}
+        /> */}
+      </Stack.Navigator>
+    </>
+  )
+}
+import React from 'react';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import Messages from './screens/Messages';
+import MessageList from './screens/MessageList';
+import Settings from '../settings/settings';
+import CreateGroup from './screens/CreateGroup';
+import ReadAnnouncements from './screens/ReadAnnouncements';
+import GroupInfo from './screens/GroupInfo';
+import CreateBlast from '../blast/CreateBlast';
+import DraftBlast from '../blast/DraftBlast';
 import { AppHeader } from '@app/components';
 // import { Settings } from 'react-native';
 
@@ -45,15 +115,7 @@ const MessagesStack = () => {
         <Stack.Screen
           name="messages"
           component={Messages}
-          options={{ headerShown: false }} 
-        />
-        <Stack.Screen 
-          name="Pinned"
-          component={Pinned}
-          options={{
-            headerShown: false,
-            presentation: 'modal'
-          }}
+          options={{title: "Messages"}}
         />
         {/* <Stack.Screen 
           name="readannouncements"
@@ -67,5 +129,7 @@ const MessagesStack = () => {
     </>
   );
 };
+
+export default MessagesStack;
 
 export default MessagesStack;
